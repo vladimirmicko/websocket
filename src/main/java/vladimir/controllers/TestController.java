6485a699a0b587b756e4c723f6af4afb8cbf6a9e
Vladimir Randjelovic <vladimirmicko@gmail.com>
2017-04-25 16:43:09 +0200

    TestController and ConterHandler changed. All handler methods work now.

--- src/main/java/vladimir/controllers/TestController.java
+++ src/main/java/vladimir/controllers/TestController.java
@@ -1,20 +1,52 @@
 package vladimir.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.WebSocketSession;
+
+import vladimir.websocket.CounterHandler;
 
 
 @RestController
 @RequestMapping(value = "/rest", produces = "application/json; charset=UTF-8")
 public class TestController {
+    
+    @Autowired
+    private CounterHandler counterHandler;
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<String> test() {
-		return new ResponseEntity<String>("OK", HttpStatus.OK);
-	}
-	
+    @RequestMapping(value = "/test", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<String>("OK", HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/session", method = RequestMethod.GET)
+    public ResponseEntity<String> session() {
+        CounterHandler a = counterHandler;
+        return new ResponseEntity<String>("Session id: "+counterHandler.getSession().getId(), HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/sessionList", method = RequestMethod.GET)
+    public ResponseEntity<List<String>> sessionList() {
+        List lista = new ArrayList();
+        for(WebSocketSession ws : counterHandler.getSessionList()){
+            lista.add("Session id: " + ws.getId());
+        }
+        return new ResponseEntity<List<String>>(lista, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/changeSession/{sessionId}", method = RequestMethod.GET)
+    public ResponseEntity<String> changeSession(@PathVariable(value = "sessionId") Integer sessionId) {
+        counterHandler.setSession(counterHandler.getSessionList().get(sessionId));
+        return new ResponseEntity<String>("Session id: "+counterHandler.getSession().getId(), HttpStatus.OK);
+    }
+    
 }
--- src/main/java/vladimir/websocket/CounterHandler.java
+++ src/main/java/vladimir/websocket/CounterHandler.java
@@ -1,5 +1,8 @@
 package vladimir.websocket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,7 +11,9 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class CounterHandler extends TextWebSocketHandler {
 
-    WebSocketSession session;
+    private WebSocketSession session;
+    private List<WebSocketSession> sessionList = new ArrayList(); 
+
 
     // This will send only to one client(most recently connected)
     public void counterIncrementedCallback(int counter) {
@@ -29,7 +34,9 @@ public class CounterHandler extends TextWebSocketHandler {
     public void afterConnectionEstablished(WebSocketSession session) {
         System.out.println("Connection established");
         this.session = session;
+        sessionList.add(session);
     }
+    
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message)
@@ -40,5 +47,23 @@ public class CounterHandler extends TextWebSocketHandler {
             System.out.println("Received:" + message.getPayload());
         }
     }
+
+	public WebSocketSession getSession() {
+		return session;
+	}
+
+	public void setSession(WebSocketSession session) {
+		this.session = session;
+	}
+
+	public List<WebSocketSession> getSessionList() {
+		return sessionList;
+	}
+
+	public void setSessionList(List<WebSocketSession> sessionList) {
+		this.sessionList = sessionList;
+	}
+    
+    
 
 }
