1ea76773dd2e7af726184789331ab4a12897f186
Vladimir Randjelovic <vladimirmicko@gmail.com>
2017-04-25 11:37:12 +0200

    Initial commit

---
+++ src/main/java/vladimir/controllers/TestController.java
@@ -0,0 +1,20 @@
+package vladimir.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/rest", produces = "application/json; charset=UTF-8")
+public class TestController {
+
+	@RequestMapping(value = "/counter", method = RequestMethod.GET, consumes = {MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("OK", HttpStatus.OK);
+	}
+	
+}
